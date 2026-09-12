b1dfe4860b2026dcb2af12d47f8421fb6243446e
naveenynick <naveenyuk97@gmail.com>
2026-09-13 02:07:45 +0530

    ci: configure GitHub Pages deployment via GitHub Actions

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Naveen-Yadav-Portfolio/',
   plugins: [
     react(),
     tailwindcss(),
